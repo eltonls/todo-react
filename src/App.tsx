2d7b0b4437afc3a608e9c6b281360eb9cb63c712
Elton Ribeiro Soares <eltonrs1999@gmail.com>
2024-08-28 07:51:32 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-slate-700 text-xl text-center">Hello, Jack Experts!</h2>
+    </>
+  )
+}
+
+export default App
